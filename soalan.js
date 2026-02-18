b47f6db0a759c6aaa3775a021f399745423c1232
naznizlabs <naznizismyusername@gmail.com>
2026-02-19 06:17:47 +0800

    Add files via upload

---
+++ soalan.js
@@ -0,0 +1,14 @@
+const masterDB = [
+    { q: "Nama asal Kota Madinah sebelum Islam.", a: "YATHRIB" },
+    { q: "Gelaran bagi Khalid bin Al-Walid.", a: "SAIFULLAH" },
+    { q: "Perang pertama dalam sejarah Islam.", a: "BADAR" },
+    { q: "Bulan diturunkan Al-Quran.", a: "RAMADAN" },
+    { q: "Maksud: Berhenti makan sebelum Subuh.", a: "IMSAK" },
+    { q: "Ibu susuan Nabi Muhammad SAW.", a: "HALIMATUSAADIAH" },
+    { q: "Kiblat pertama sebelum Baitullah.", a: "BAITULMAQDIS" },
+    { q: "Sahabat paling banyak riwayat hadis.", a: "ABUHURAIRAH" },
+    { q: "Perjalanan Nabi ke Sidratul Muntaha.", a: "ISRAKMIKRAJ" },
+    { q: "Pedang milik Saidina Ali.", a: "ZULFIQAR" }
+];
+
+
