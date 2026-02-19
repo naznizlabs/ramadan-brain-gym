a260745288667304a047fc67813452c0f3e8f8c3
naznizlabs <naznizismyusername@gmail.com>
2026-02-19 22:50:04 +0800

    Add files via upload

--- soalan.js
+++ soalan.js
@@ -1,14 +1,26 @@
-const masterDB = [
-    { q: "Nama asal Kota Madinah sebelum Islam.", a: "YATHRIB" },
-    { q: "Gelaran bagi Khalid bin Al-Walid.", a: "SAIFULLAH" },
-    { q: "Perang pertama dalam sejarah Islam.", a: "BADAR" },
-    { q: "Bulan diturunkan Al-Quran.", a: "RAMADAN" },
-    { q: "Maksud: Berhenti makan sebelum Subuh.", a: "IMSAK" },
-    { q: "Ibu susuan Nabi Muhammad SAW.", a: "HALIMATUSAADIAH" },
-    { q: "Kiblat pertama sebelum Baitullah.", a: "BAITULMAQDIS" },
-    { q: "Sahabat paling banyak riwayat hadis.", a: "ABUHURAIRAH" },
-    { q: "Perjalanan Nabi ke Sidratul Muntaha.", a: "ISRAKMIKRAJ" },
-    { q: "Pedang milik Saidina Ali.", a: "ZULFIQAR" }
-];
-
-
+const masterDB = {
+    bm: [
+        { q: "Nama asal bagi Surah Al-Fatihah (Pembukaan).", a: "UMMULKITAB" },
+        { q: "Gelaran bagi isteri Nabi Muhammad, Saiditina Aisyah.", a: "HUMAIRA" },
+        { q: "Ibu kota empayar Islam semasa zaman Khalifah Harun Al-Rasyid.", a: "BAGHDAD" },
+        { q: "Nama gunung tempat berlakunya Perang Uhud.", a: "UHUD" },
+        { q: "Gelaran bagi Saidina Uthman bin Affan.", a: "ZUNNURAIN" },
+        { q: "Perang terakhir yang disertai oleh Rasulullah SAW.", a: "TABUK" },
+        { q: "Tokoh yang melaungkan Azan pertama dalam Islam.", a: "BILALRABAH" },
+        { q: "Nama perjanjian yang membawa kepada Pembukaan Kota Makkah.", a: "HUDAIBIYAH" },
+        { q: "Nama pedang terkenal milik Khalid bin Al-Walid.", a: "ZULFIQAR" },
+        { q: "Bapa saudara Nabi yang syahid dalam Perang Uhud.", a: "HAMZAH" }
+    ],
+    en: [
+        { q: "The original name of Surah Al-Fatihah.", a: "UMMULKITAB" },
+        { q: "Nickname given to Prophet's wife, Aisha RA.", a: "HUMAIRA" },
+        { q: "Capital of Islamic Empire during Harun Al-Rashid era.", a: "BAGHDAD" },
+        { q: "The mountain where the Battle of Uhud took place.", a: "UHUD" },
+        { q: "The title given to Caliph Uthman bin Affan.", a: "ZUNNURAIN" },
+        { q: "The last battle joined by Prophet Muhammad SAW.", a: "TABUK" },
+        { q: "The first person to call the Adhan (Call to prayer).", a: "BILALRABAH" },
+        { q: "The treaty that led to the Conquest of Makkah.", a: "HUDAIBIYAH" },
+        { q: "The famous sword of Khalid bin Al-Walid.", a: "ZULFIQAR" },
+        { q: "The Prophet's uncle who was martyred in Uhud.", a: "HAMZAH" }
+    ]
+};
